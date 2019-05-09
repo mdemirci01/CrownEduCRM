7b4f7fcc81bfedaff37c3a9caedad42b6c0454a8
Section1 <Section1@WISSEN>
2019-05-09 17:11:22 +0300

    identity scaffolded

---
+++ CrownEduCRM.Admin/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CrownEduCRM.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CrownEduCRM.Admin.Areas.Identity.IdentityHostingStartup))]
+namespace CrownEduCRM.Admin.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
