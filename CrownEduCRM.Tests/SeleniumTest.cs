d59505fe5059a40efafeedc76dc18954602aa5e6
Section1 <Section1@WISSEN>
2019-05-10 18:01:43 +0300

    selenium testi eklendi

---
+++ CrownEduCRM.Tests/SeleniumTest.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CrownEduCRM.Tests
+{
+    public class SeleniumTest
+    {
+        private RemoteWebDriver driver;
+        [SetUp]
+        public void SetUp()
+        {
+            string driverDirectory = Path.Combine(Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(Environment.CurrentDirectory))), "Drivers");
+            driver = new ChromeDriver(driverDirectory);
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://www.apsiyon.com");
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var title = driver.Title;
+            Assert.AreEqual("Apsiyon - Site Yönetim Yazılımı, Apartman Yönetim Programı", title);
+        }
+
+        [TearDown]
+        public void Close()
+        {
+            driver.Quit();
+        }
+    }
+}
